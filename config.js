4414209a784f5cb77b0e58745a5135e2697c6d2c
htoooth <ht.anglenx@gmail.com>
2017-12-20 17:32:18 +0800

    fix

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
+const HOUR = 60 * 60 * 1000;
+
 module.exports = {
   key: 'etnk8ggPHFhD1vidxJDoo9cBSzBmePyPMj788nHdYBnnC3ycaJsuM35cmeMBmjZgLMgMtAWWXrBwbK78yc3bkXinAUJAXJkukU',
-  time: 2 * 60 * 60 * 1000
+  time: 2 * HOUR
 };
